da7915f15e861c13a373e2b8289b260b1d2fb131
Reynaldo <reycastano97@gmail.com>
2022-10-18 17:26:31 -0600

    Adding an update and a JS file to alert when submit button has been clicked

---
+++ main.js
@@ -0,0 +1,10 @@
+window.addEventListener("load", function(){
+    document.getElementById("submit-button").addEventListener("click", function(){
+        alert("Thank you for submitting");
+    })
+})
+
+/* This function works to show the alert the user has submitted information
+Email and Name.
+
+*/
